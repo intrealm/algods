74fb1681a08ed323ca5b535abee864586ea998a8
Sourav Pathak <soupatha@publicisgroupe.net>
2019-05-25 19:04:39 +0530

    ordered traversal of the list done

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,5 +1,6 @@
 
 import com.sun.istack.internal.NotNull;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -56,7 +57,7 @@ public class BinaryTree {
         return new BinaryTreeNode(value);
     }
 
-    private List<Integer> getorderedList(@NotNull BinaryTreeNode currNode,LinkedList<Integer> list)
+    private void getorderedList(@NotNull BinaryTreeNode currNode,LinkedList<Integer> list)
     {
 
         //navigateLeft
@@ -70,30 +71,97 @@ public class BinaryTree {
         {
             getorderedList(currNode.getRight(),list);
         }
+    }
 
+    private void getPostorderedList(@NotNull BinaryTreeNode currNode,LinkedList<Integer> list)
+    {
 
-        //middle
+        //navigateLeft
+        if(!Objects.isNull(currNode.getLeft()))
+        {
+            getPostorderedList(currNode.getLeft(),list);
+        }
+        if(!Objects.isNull(currNode.getRight()))
+        {
+            getPostorderedList(currNode.getRight(),list);
+        }
+        list.add(currNode.getVal());
+    }
+    private void getPreorderedList(@NotNull BinaryTreeNode currNode,LinkedList<Integer> list)
+    {
+        list.add(currNode.getVal());
+        //navigateLeft
+        if(!Objects.isNull(currNode.getLeft()))
+        {
+            getPreorderedList(currNode.getLeft(),list);
+        }
+        if(!Objects.isNull(currNode.getRight()))
+        {
+            getPreorderedList(currNode.getRight(),list);
+        }
+    }
 
-        //right
+    private void getTwistedOrderedList(@NotNull BinaryTreeNode currNode,LinkedList<Integer> list,boolean twist)
+    {
+        list.add(currNode.getVal());
+        if(!twist)
+        {
+            if(!Objects.isNull(currNode.getLeft()))
+            {
+                getTwistedOrderedList(currNode.getLeft(),list, true);
+            }
 
+            if(!Objects.isNull(currNode.getRight()))
+            {
+                getTwistedOrderedList(currNode.getRight(),list,true);
+            }
+        }
+        else
+        {
+            if(!Objects.isNull(currNode.getRight()))
+            {
+                getTwistedOrderedList(currNode.getRight(),list,false);
+            }
+            if(!Objects.isNull(currNode.getLeft()))
+            {
+                getTwistedOrderedList(currNode.getLeft(),list,false);
+            }
+        }
     }
 
     public List<Integer> getInOrderedList()
     {
         if(head==null)
             return Collections.EMPTY_LIST;
-        List<Integer> list = getorderedList(this.head,new LinkedList<Integer>());
+        LinkedList<Integer> list = new LinkedList<Integer>();
+                getorderedList(this.head,list);
         return list;
     }
 
-    public void printPreOrder()
+    public List<Integer> getTwistedOrderedList()
     {
-
+        if(head==null)
+            return Collections.EMPTY_LIST;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        getTwistedOrderedList(this.head,list,false);
+        return list;
     }
 
+    public List<Integer> getPreOrderedList() {
+        if(head==null)
+            return Collections.EMPTY_LIST;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        getPreorderedList(this.head,list);
+        return list;
+    }
 
-    public void printPostOrder()
+    public List<Integer> getPostOrderedList()
     {
+        if(head==null)
+            return Collections.EMPTY_LIST;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        getPostorderedList(this.head,list);
+        return list;
 
     }
 
@@ -102,16 +170,32 @@ public class BinaryTree {
 
     }
 
+    String actualTrueTreeFormed()
+    {
+        BinaryTreeNode node = this.head;
+        StringBuffer sb = new StringBuffer();
+
+        if(node!=null)
+        publishToBuffer(node,sb);
+                return sb.toString();
+    }
+
+    final String seperator = System.getProperty("line.separator");
+
+    void publishToBuffer(BinaryTreeNode node,StringBuffer sb)
+    {
+        sb.append(seperator);
+        sb.append(node.getVal());
+        if(node.getLeft()!=null)
+        publishToBuffer(node.getLeft(),sb);
+        if(node.getRight()!=null)
+        publishToBuffer(node.getRight(),sb);
+    }
+
+
 
     public String toString()
     {
-        List<Integer> list = getInOrderedList();
-
-        StringBuffer sb = new StringBuffer();
-
-        list.forEach(str ->
-                sb.append("--"+str)
-        );
-        sb.toString();
+     return  actualTrueTreeFormed();
     }
 }
--- src/test/java/BinaryTreeTest.java
+++ src/test/java/BinaryTreeTest.java
@@ -1,4 +1,8 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class BinaryTreeTest {
 
@@ -13,7 +17,46 @@ public class BinaryTreeTest {
         tree.addValue(15);
         tree.addValue(7);
 
-        System.out.println(tree);
+        tree.addValue(5);
+        tree.addValue(4);
+        tree.addValue(22);
+        tree.addValue(13);
+        tree.addValue(70);
+        tree.addValue(8);
+
+        //      3
+        //        9
+        //      7    20
+        //    5  8  15   22
+        //  4      13       70
+        //
+        //
+        StringBuffer testString = new StringBuffer();
+        Consumer con = System.out::print;
+        Consumer con2 = (x)->{
+            System.out.print(",");
+            testString.append(x).append(",");
+        };
+
+        List<Integer> lst = tree.getInOrderedList();
+        lst.forEach(con.andThen(con2));
+
+        Assert.assertEquals("3,4,5,7,8,9,13,15,20,22,70,",testString.toString());
+
+        testString.setLength(0);
+        System.out.println();
+        List<Integer> preOrder= tree.getPreOrderedList();
+        preOrder.forEach(con.andThen(con2));
+
+        Assert.assertEquals("3,9,7,5,4,8,20,15,13,22,70,",testString.toString());
+
+        testString.setLength(0);
+        System.out.println();
+        List<Integer> postOrder= tree.getPostOrderedList();
+        postOrder.forEach(con.andThen(con2));
+
+        Assert.assertEquals("4,5,8,7,13,15,70,22,20,9,3,",testString.toString());
+
 
     }
 }
