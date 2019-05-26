1fa79af682f74c78f2ca0ec01052c7b930f7ee16
sourav <sourav.pathak@sapient.com>
2019-05-26 18:43:35 +0530

    min cost

--- src/main/java/array2d/Array2D.java
+++ src/main/java/array2d/Array2D.java
@@ -4,6 +4,31 @@ public class Array2D {
 
     private int arr[][];
 
+   static  class RepThisOne
+    {
+        private int arr[][];
+        public RepThisOne(int arr[][])
+        {
+            this.arr = arr;
+        }
+
+        @Override
+        public String toString() {
+            StringBuffer sb = new StringBuffer();
+            int n = arr.length;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    sb.append(String.format("##%2d",arr[i][j]));
+                }
+                sb.append(System.getProperty("line.separator"));
+            }
+
+            return sb.toString();
+        }
+    }
+
 
     public Array2D(int n)
     {
@@ -31,6 +56,8 @@ public class Array2D {
         //prepare a cost arr
         int costarr[][] = new int[arr.length][arr[0].length];
 
+        RepThisOne origin = new RepThisOne(this.arr);
+        RepThisOne cost = new RepThisOne(costarr);
         //first row costs the same
         //copying - start
         costarr[0][0]=arr[0][0];
@@ -42,10 +69,67 @@ public class Array2D {
         // start painting columns and rows with minimum cost
         for(int i=1;i<arr.length;i++)
         {
-            
+            for(int j=1;j<arr.length;j++)
+            {
+                //0 2    0 2    or      0 2
+                //3 2    3 4            3 5
+                if(arr[i][j]+costarr[i][j-1] > arr[i][j]+costarr[i-1][j])
+                    costarr[i][j]=arr[i][j]+costarr[i-1][j];
+                else
+                    costarr[i][j]=arr[i][j]+costarr[i][j-1];
+            }
+
         }
-        return 0;
+        return costarr[arr.length-1][arr[0].length-1];
     }
 
+
+    public int minimumCostWithBlockers()
+    {
+        //prepare a cost arr
+        int costarr[][] = new int[arr.length][arr[0].length];
+
+        RepThisOne origin = new RepThisOne(this.arr);
+        RepThisOne cost = new RepThisOne(costarr);
+        //first row costs the same
+        //copying - start
+        costarr[0][0]=arr[0][0];
+        for(int i=1;i<arr.length;i++)
+        {
+            costarr[0][i]=costarr[0][i-1]+arr[0][i];
+            costarr[i][0]=costarr[i-1][0]+arr[i][0];
+        }
+        // start painting columns and rows with minimum cost
+        for(int i=1;i<arr.length;i++)
+        {
+            for(int j=1;j<arr.length;j++)
+            {
+                if(arr[i][j]<0)
+                    costarr[i][j]=-1;
+
+                if(costarr[i][j-1]==-1)
+                {
+                    costarr[i][j]=costarr[i-1][j];
+                }
+                else if(costarr[i-1][j]==-1)
+                {
+                    costarr[i][j]=costarr[i][j-1];
+                }
+                else
+                {
+                    //0 2    0 2    or      0 2
+                    //3 2    3 4            3 5
+                    if(arr[i][j]+costarr[i][j-1] > arr[i][j]+costarr[i-1][j])
+                        costarr[i][j]=arr[i][j]+costarr[i-1][j];
+                    else
+                        costarr[i][j]=arr[i][j]+costarr[i][j-1];
+                }
+
+
+            }
+
+        }
+        return costarr[arr.length-1][arr[0].length-1];
+    }
 
 }
--- src/test/java/array2d/MinumumCost.java
+++ src/test/java/array2d/MinumumCost.java
@@ -1,11 +1,12 @@
 package array2d;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MinumumCost {
 
     @Test
-    public void getMinimumCost()
+    public void getMinimumCost_base_version()
     {
         //0  1  5 7 9
         //45 5  2 1 13
@@ -20,6 +21,26 @@ public class MinumumCost {
         arr.setValueAtPos(3,0,4);arr.setValueAtPos(3,1,7);arr.setValueAtPos(3,2,2);arr.setValueAtPos(3,3,7);arr.setValueAtPos(3,4,1);
         arr.setValueAtPos(4,0,1);arr.setValueAtPos(4,1,2);arr.setValueAtPos(4,2,20);arr.setValueAtPos(4,3,1);arr.setValueAtPos(4,4,2);
 
-        arr.minimumCost();
+        Assert.assertEquals(arr.minimumCost(),21);
       }
+
+
+    @Test
+    public void getMinimumCost_blockers()
+    {
+        //0 -1  5 7 9
+        //45 5 -2 1 13
+        //0  3 -1 4 9
+        //4  7  2 7 1
+        //1  2 20 1 2
+
+        Array2D arr = new Array2D(5);
+        arr.setValueAtPos(0,0,0);arr.setValueAtPos(0,1,-1);arr.setValueAtPos(0,2,5);arr.setValueAtPos(0,3,7);arr.setValueAtPos(0,4,9);
+        arr.setValueAtPos(1,0,45);arr.setValueAtPos(1,1,5);arr.setValueAtPos(1,2,-2);arr.setValueAtPos(1,3,1);arr.setValueAtPos(1,4,13);
+        arr.setValueAtPos(2,0,0);arr.setValueAtPos(2,1,3);arr.setValueAtPos(2,2,-1);arr.setValueAtPos(2,3,4);arr.setValueAtPos(2,4,9);
+        arr.setValueAtPos(3,0,4);arr.setValueAtPos(3,1,7);arr.setValueAtPos(3,2,2);arr.setValueAtPos(3,3,7);arr.setValueAtPos(3,4,1);
+        arr.setValueAtPos(4,0,1);arr.setValueAtPos(4,1,2);arr.setValueAtPos(4,2,20);arr.setValueAtPos(4,3,1);arr.setValueAtPos(4,4,2);
+
+        Assert.assertEquals(arr.minimumCost(),67);
+    }
 }
